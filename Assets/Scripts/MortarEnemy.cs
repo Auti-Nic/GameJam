40fea3a05f1a652eb6ae1a66393cecdef249bfba
Jack Coggins <voidawake90@gmail.com>
2021-09-05 16:47:58 +1000

    Mortar enemy

---
+++ Assets/Scripts/MortarEnemy.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+
+namespace DefaultNamespace
+{
+    public class MortarEnemy : EnemyAI
+    {
+        private Animator animator;
+        private AnimationEventHandler events;
+
+        [SerializeField] private Vector3 launchForce;
+
+        protected override void Awake()
+        {
+            base.Awake();
+            
+            animator = GetComponentInChildren<Animator>();
+            events = GetComponentInChildren<AnimationEventHandler>();
+
+            if (events)
+                events.OnAnimationEvent += OnAnimationEvent;
+        }
+
+        private void OnAnimationEvent(string eventName)
+        {
+            if (eventName == "Fire")
+                FireMortar();
+        }
+
+        private void FireMortar()
+        {
+            var newMortarBullet = Instantiate(projectile, transform.position + gunPosition, Quaternion.identity);
+            
+            newMortarBullet.GetComponent<Rigidbody>().AddForce(transform.TransformPoint(launchForce));
+        }
+
+        protected override void Fire()
+        {
+            animator.SetTrigger("Firing");
+        }
+    }
+}
